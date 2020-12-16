dd0007cfa6ee1db446e02ab102cbff302fea1ba3
LpmRaven <liamraven@gmail.com>
2020-12-16 13:10:28 +1100

    adds logs to determine if location is passed back from origin

--- handler.js
+++ handler.js
@@ -1,18 +1,33 @@
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event) => {
     const response = event.Records[0].cf.response;
 
     try {
+        const uri = request.uri;
+        const parsedPath = path.parse(request.uri);
+
+        console.log(`response.headers['location']`, response.headers['location']);
+        console.log('request.uri', request.uri);
+
 
         if (response.status === '302') {
+            console.log('change to 301');
             response.status = '301';
             response.statusDescription = 'Moved Permanently'
         }
 
-        callback(null, response);
+        // if (parsedPath.base === 'index.html') {
+        //     response.uri = response.uri.slice(0, -11);
+        // }
+
+        // if (response.uri.slice(-1) === "/") {
+        //     response.uri = response.uri.slice(0, -1);
+        // }
+
+        return response;
 
     } catch (err) {
         console.error(err);
-        callback(null, response);
+        return response;
     }
 };
---
+++ helpers/get-custom-response-with-url.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+const getCustomResponseWithUrl = (uri, statusCode) => {
+    const parsedPath = path.parse(uri);
+    let newUri;
+    if (parsedPath.base !== 'index.html') { // Allows you to specify index.html as default root object in CloudFront
+        newUri = path.join(parsedPath.dir, parsedPath.base, 'index.html');
+    } else {
+        newUri = path.join(parsedPath.dir, parsedPath.base);
+    }
+
+
+    if (statusCode === "302") {
+        return (
+            {
+                status: '302',
+                statusDescription: 'Found',
+                headers: {
+                    location: [{
+                        key: 'Location',
+                        value: newUri,
+                    }],
+                    'cache-control': [{
+                        key: 'Cache-Control',
+                        value: "max-age=3600"
+                    }],
+                },
+            }
+        )
+    }
+
+
+};
+
+module.exports = {
+    getCustomResponseWithUrl
+}
