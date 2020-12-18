15704a82d144507e432149c7ff2fea8ddb2a8ae2
LpmRaven <liamraven@gmail.com>
2020-12-18 14:18:59 +1100

    adds image ext to cache control

--- helpers/get-cache-control.js
+++ helpers/get-cache-control.js
@@ -5,14 +5,35 @@ const getCacheControl = (request, response) => {
     if (request.uri) {
         const parsedPath = path.parse(request.uri);
 
-        if (parsedPath.ext === ".js" || parsedPath.ext === ".css" || parsedPath.dir.includes("/static")) {
+        if (
+            parsedPath.ext === ".js" ||
+            parsedPath.ext === ".css" ||
+            parsedPath.ext === ".json" ||
+            parsedPath.ext === ".woff" ||
+            parsedPath.ext === ".woff2" ||
+            parsedPath.ext === ".ttf" ||
+            parsedPath.ext === ".otf" ||
+            parsedPath.ext === ".eot" ||
+            parsedPath.ext === ".jpg" ||
+            parsedPath.ext === ".jpeg" ||
+            parsedPath.ext === ".png" ||
+            parsedPath.ext === ".gif" ||
+            parsedPath.ext === ".svg" ||
+            parsedPath.ext === ".ico" ||
+            parsedPath.dir.includes("/static")
+        ) {
             response.headers['cache-control'] = [{
                 key: 'Cache-Control',
                 value: 'cache-control: public, max-age=31536000, immutable'
             }];
         }
 
-        if (parsedPath.ext === ".html" || parsedPath.name === "sw" && parsedPath.ext === ".js" || parsedPath.base === "page-data.json" || parsedPath.base === "app-data.json") {
+        if (
+            parsedPath.ext === ".html" ||
+            parsedPath.name === "sw" && parsedPath.ext === ".js" ||
+            parsedPath.base === "page-data.json" ||
+            parsedPath.base === "app-data.json"
+        ) {
             response.headers['cache-control'] = [{
                 key: 'Cache-Control',
                 value: 'public, max-age=0, must-revalidate'
